385bf6fe79542c9c106478f4dd7bbdf0f2804346
williamneo <williamneokh@gmail.com>
2022-03-31 01:43:18 +0800

    Modify code done

--- fallthrough.go
+++ fallthrough.go
@@ -1,36 +1,41 @@
+package main
+
+import "fmt"
+
 //Guess what happens
 //Modify the codes to see the outcome
+func main() {
+	switch {
+	case 20 > 40:
+		fmt.Println("20 > 40")
+	case 10 > 1:
+		fallthrough
+	case 1 > 10:
+		fmt.Println("1>10")
+	default:
+		fmt.Println("None 1")
+	}
 
-switch {
-case 20 > 40:
-	fmt.Println("20 > 40")
-case 10 > 1:
-	fallthrough
-case 1 > 10:
-	fmt.Println("1>10")
-default:
-	fmt.Println("None 1")
+	switch {
+	case 10 > 11:
+		fmt.Println("10 > 11")
+	case 5 > 1:
+		//fallthrough
+	case 1 > 10:
+		fmt.Println("1 > 10")
+	default:
+		fmt.Println("None 2")
+	}
+
+	switch {
+	case 20 > 21:
+		fmt.Println("20 > 21")
+	case 10 > 1:
+		break
+	case 20 > 10:
+		fmt.Println("20 > 10")
+	default:
+		fmt.Println("None 3")
+	}
+
 }
-
-switch {
-case 10 > 11:
-	fmt.Println("10>11")
-case 5 > 1:
-	//fallthrough
-case 1 > 10:
-	fmt.Println("1>10")
-default:
-	fmt.Println("None 2")
-}
-
-switch {
-case 20 > 21:
-	fmt.Println("20>21")
-case 10 > 1:
-	break
-case 20 > 10:
-	fmt.Println("20>10")
-default:
-	fmt.Println("None")
-}
-
